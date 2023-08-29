f97bd2f1d099eee0b186de5b3b48a77cc771f19e
Lautaro Gruss <lautapercuspain@gmail.com>
2023-08-29 11:05:05 -0700

    added a card comp

---
+++ pages/components/Card.js
@@ -0,0 +1,13 @@
+const Card = (props) => {
+    return (
+      <div className="card">
+        <img src={props.imageUrl} alt={props.title} />
+        <div className="card-content">
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </div>
+      </div>
+    );
+  };
+  
+export default Card;
