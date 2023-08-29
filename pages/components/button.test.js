9c1aff4b8819f62f0fcf91aabda703ed4a6eb4b4
Lautaro Gruss <lautapercuspain@gmail.com>
2023-08-28 21:15:28 -0700

    Update button.test.js

--- pages/components/button.test.js
+++ pages/components/button.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Button from '../Button';
+import Button from './Button';
 
 afterEach(cleanup);
 
