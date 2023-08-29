b6af7802c5a46f20a525e969a66bef79774c0a67
Lautaro Gruss <lautapercuspain@gmail.com>
2023-08-29 11:32:25 -0700

    Update Card.js

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -1,7 +1,7 @@
 const Card = (props) => {
     return (
       <div className="card">
-        <img src={props.imageUrl} alt={props.title} />
+        {props.imageUrl && <img src={props.imageUrl} alt={props.title} />}
         <div className="card-content">
           <h2>{props.title}</h2>
           <p>{props.description}</p>
