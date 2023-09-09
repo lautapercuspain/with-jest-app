3567a93029c4e4b8cd9a4f32d753e620b20a4f91
Lautaro Gruss <lautapercuspain@gmail.com>
2023-09-09 03:35:09 -0700

    Update timer.test.js

--- pages/components/timer.test.js
+++ pages/components/timer.test.js
@@ -1,7 +1,3 @@
-Sure! Here's an example unit test using Jest and React Testing Library for the Timer component from the given URL:
-
-```jsx
-// Timer.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Timer from './Timer';
@@ -12,8 +8,3 @@ test('renders Timer component', () => {
   const timerElement = screen.getByText(/Timer:/i);
   expect(timerElement).toBeInTheDocument();
 });
-```
-
-In this test, we render the Timer component and then use `screen.getByText` to find the element with the text "Timer:". We then use `expect` to check if the element is in the document.
-
-You can run this test using Jest, and it will check if the Timer component renders correctly.
