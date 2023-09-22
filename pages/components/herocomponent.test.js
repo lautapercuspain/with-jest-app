745fc227fcb14c74e8c33e932268e6c3da06b6b5
Lautaro Gruss <lautapercuspain@gmail.com>
2023-09-22 10:44:48 -0600

    Delete pages/components/herocomponent.test.js

--- pages/components/herocomponent.test.js
+++
@@ -1,1 +0,0 @@
-Sorry, the provided code snippet is "undefined" which is not sufficient to create a unit test. Can you please provide a valid React component so I can generate the corresponding unit tests?
